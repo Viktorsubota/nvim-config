55b2d8fde1f63af6137f9d5ff5d727d906a0cf63
Viktor Subota <victorsubota@gmail.com>
2024-03-09 23:11:57 +0100

    Remove set icons

--- lua/vsubota/plugins/cmp.lua
+++ lua/vsubota/plugins/cmp.lua
@@ -1,33 +1,3 @@
---   פּ ﯟ   some other good icons
-local kind_icons = {
-	Text = "",
-	Method = "m",
-	Function = "",
-	Constructor = "",
-	Field = "",
-	Variable = "",
-	Class = "",
-	Interface = "",
-	Module = "",
-	Property = "",
-	Unit = "",
-	Value = "",
-	Enum = "",
-	Keyword = "",
-	Snippet = "",
-	Color = "",
-	File = "",
-	Reference = "",
-	Folder = "",
-	EnumMember = "",
-	Constant = "",
-	Struct = "",
-	Event = "",
-	Operator = "",
-	TypeParameter = "",
-}
--- find more here: https://www.nerdfonts.com/cheat-sheet
-
 return {
 	{
 		"hrsh7th/cmp-nvim-lsp",
