e26ae3082e925dd6aa5e9a008fa04c218176e298
Viktor Subota <viktor.subota@c.delinea.com>
2024-04-04 10:12:24 +0200

    Update filepath appearance in lualine

--- lua/vsubota/plugins/lualine.lua
+++ lua/vsubota/plugins/lualine.lua
@@ -2,6 +2,7 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
+		local current_session_name = require("auto-session.lib").current_session_name
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
@@ -16,18 +17,36 @@ return {
 				always_divide_middle = true,
 				globalstatus = false,
 				refresh = {
-					statusline = 1000,
-					tabline = 1000,
-					winbar = 1000,
+					statusline = 2000,
+					tabline = 2000,
+					winbar = 2000,
 				},
 			},
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { "filename" },
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+						fmt = function(input)
+							local line = string.gsub(input, "oil://", ""):gsub("/$", ""):gsub("/ ", " ")
+
+							local project = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+							local path = vim.fn.fnamemodify(line, ":.:h")
+							local filename = vim.fn.fnamemodify(line, ":t")
+
+							local parts = { project, path, filename }
+							return table.concat(parts, " ➜ ")
+						end,
+						color = function(_)
+							return { fg = "#92b3f4" }
+						end,
+					},
+				},
 				lualine_x = { "filetype" },
 				lualine_y = { "progress", "location" },
-				lualine_z = { require("auto-session.lib").current_session_name },
+				lualine_z = { current_session_name },
 			},
 			inactive_sections = {
 				lualine_a = {},
