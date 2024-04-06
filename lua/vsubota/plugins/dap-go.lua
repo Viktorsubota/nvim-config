9f2257d9d0777be3a3e752b034c48e2b7b05e495
Viktor Subota <victorsubota@gmail.com>
2024-04-06 23:09:44 +0200

    Add lazy-loading commands for DAP plugins

--- lua/vsubota/plugins/dap-go.lua
+++ lua/vsubota/plugins/dap-go.lua
@@ -1,6 +1,19 @@
 return {
 	"leoluz/nvim-dap-go",
 	ft = "go",
+	cmd = {
+		"DapContinue",
+		"DapLoadLaunchJSON",
+		"DapRestartFrame",
+		"DapSetLogLevel",
+		"DapShowLog",
+		"DapStepInto",
+		"DapStepOut",
+		"DapStepOver",
+		"DapTerminate",
+		"DapToggleBreakpoint",
+		"DapToggleRepl",
+	},
 	dependencies = {
 		"mfussenegger/nvim-dap",
 		"rcarriga/nvim-dap-ui",
