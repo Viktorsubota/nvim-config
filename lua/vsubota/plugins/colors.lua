2bed2b009f691481172fcfb3edd3fe37abc71c1e
Viktor Subota <victorsubota@gmail.com>
2024-04-08 13:34:37 +0200

    Implement custom higlight in native way

--- lua/vsubota/plugins/colors.lua
+++ lua/vsubota/plugins/colors.lua
@@ -1,20 +1,19 @@
 local custom_highlights = {
-	["catppuccin-mocha"] = function(colors)
+	["catppuccin-macchiato"] = function(colors)
 		return {
-			Normal = { bg = "none" },
-			NormalFloat = { bg = "none" },
-
 			-- Number Line color
 			LineNr = { fg = colors.overlay0 },
 			-- Cursor Line color
 			CursorLineNr = { fg = colors.saphire },
 
 			-- Customize the highlighting color to grey for current and lenth lines
-			CursorLine = { bg = colors.surface0 },
-			ColorColumn = { bg = colors.surface0 },
+			CursorLine = { bg = "#303347" },
+			ColorColumn = { bg = "#303347" },
+			-- CursorLine = { bg = colors.surface0 },
+			-- ColorColumn = { bg = colors.surface0 },
 
 			-- Function IBL line
-			CurrentScope = { fg = colors.surface1 },
+			IblScope = { fg = colors.surface1 },
 
 			-- Gitsign colors for added, changed and deleted lines
 			GitSignsAdd = { fg = colors.blue },
@@ -22,9 +21,9 @@ local custom_highlights = {
 			GitSignsDelete = { fg = colors.red },
 
 			-- Collors for refs autohiglights
-			LspReferenceRead = { cterm = { bold = true }, ctermbg = 135, bg = colors.surface1 },
-			LspReferenceText = { cterm = { bold = true }, ctermbg = 135, bg = colors.surface1 },
-			LspReferenceWrite = { cterm = { bold = true }, ctermbg = 135, bg = colors.surface1 },
+			LspReferenceRead = { cterm = { bold = true }, ctermbg = 135, bg = colors.surface0 },
+			LspReferenceText = { cterm = { bold = true }, ctermbg = 135, bg = colors.surface0 },
+			LspReferenceWrite = { cterm = { bold = true }, ctermbg = 135, bg = colors.surface0 },
 		}
 	end,
 }
@@ -34,9 +33,17 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
-		local current_colorscheme = "catppuccin-mocha"
+		local current_colorscheme = "catppuccin-macchiato"
+		-- local current_colorscheme = "catppuccin-latte"
 
-		require("catppuccin").setup({ custom_highlights = custom_highlights["catppuccin-mocha"] })
+		require("catppuccin").setup({
+			custom_highlights = custom_highlights[current_colorscheme],
+			dim_inactive = {
+				enabled = true, -- dims the background color of inactive window
+				shade = "dark",
+				percentage = 0.25, -- percentage of the shade to apply to the inactive window
+			},
+		})
 
 		vim.cmd.colorscheme(current_colorscheme)
 	end,
