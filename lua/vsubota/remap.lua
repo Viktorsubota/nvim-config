ee14b6ce3d3ffb325a9a7f5ce638e2bf638bf507
Viktor Subota <viktor.subota@c.delinea.com>
2024-05-03 19:39:54 +0200

    Some fixes

--- lua/vsubota/remap.lua
+++ lua/vsubota/remap.lua
@@ -24,8 +24,11 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
+
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
