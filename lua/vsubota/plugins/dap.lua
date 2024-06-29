74ba59d188306eaf4ed405616d33f7f7352b5725
John Doe <junk1233messages@gmail.com>
2024-06-29 15:51:38 +0200

    Fix DAP

--- lua/vsubota/plugins/dap.lua
+++ lua/vsubota/plugins/dap.lua
@@ -1,18 +1,19 @@
 return {
 	"mfussenegger/nvim-dap",
-	cmd = {
-		"DapContinue",
-		"DapLoadLaunchJSON",
-		"DapRestartFrame",
-		"DapSetLogLevel",
-		"DapShowLog",
-		"DapStepInto",
-		"DapStepOut",
-		"DapStepOver",
-		"DapTerminate",
-		"DapToggleBreakpoint",
-		"DapToggleRepl",
-	},
+	event = "VeryLazy",
+	-- cmd = {
+	-- 	"DapContinue",
+	-- 	"DapLoadLaunchJSON",
+	-- 	"DapRestartFrame",
+	-- 	"DapSetLogLevel",
+	-- 	"DapShowLog",
+	-- 	"DapStepInto",
+	-- 	"DapStepOut",
+	-- 	"DapStepOver",
+	-- 	"DapTerminate",
+	-- 	"DapToggleBreakpoint",
+	-- 	"DapToggleRepl",
+	-- },
 	config = function()
 		vim.keymap.set("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>")
 	end,
