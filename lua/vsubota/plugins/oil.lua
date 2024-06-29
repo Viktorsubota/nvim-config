7b588e53727635555a5fd59f795feaeb9951f1a9
John Doe <junk1233messages@gmail.com>
2024-06-29 15:52:03 +0200

    Improve oil and harpoon

--- lua/vsubota/plugins/oil.lua
+++ lua/vsubota/plugins/oil.lua
@@ -1,19 +1,28 @@
 return {
 	"stevearc/oil.nvim",
 	opts = {},
+	keys = {
+		{ "-", "<cmd>Oil<CR>", mode = { "n" } },
+		{ "<leader>pv", "<cmd>Oil<CR>", mode = { "n" } },
+	},
+	lazy = false,
 	-- Optional dependencies
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("oil").setup({
+			default_file_explorer = true,
+			delete_to_trash = true,
+			skip_confirm_for_simple_edits = true,
 			view_options = {
-				-- Show files and directories that start with "."
 				show_hidden = true,
+				natural_order = true,
+				is_always_hidden = function(name, _)
+					return name == ".." or name == ".git"
+				end,
+			},
+			win_options = {
+				wrap = true,
 			},
 		})
-
-		local oil = require("oil")
-		vim.keymap.set("n", "<leader>pv", function()
-			oil.open()
-		end)
 	end,
 }
