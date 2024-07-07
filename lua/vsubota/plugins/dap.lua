ea0c4eb1ebd4d7bb7a4eb1263619b6ab21953642
Viktor Subota <viktor.subota@c.delinea.com>
2024-07-07 23:24:01 +0200

    Add Telescope extention, add file filtering to live grep

--- lua/vsubota/plugins/dap.lua
+++ lua/vsubota/plugins/dap.lua
@@ -1,20 +1,37 @@
 return {
 	"mfussenegger/nvim-dap",
-	event = "VeryLazy",
-	-- cmd = {
-	-- 	"DapContinue",
-	-- 	"DapLoadLaunchJSON",
-	-- 	"DapRestartFrame",
-	-- 	"DapSetLogLevel",
-	-- 	"DapShowLog",
-	-- 	"DapStepInto",
-	-- 	"DapStepOut",
-	-- 	"DapStepOver",
-	-- 	"DapTerminate",
-	-- 	"DapToggleBreakpoint",
-	-- 	"DapToggleRepl",
-	-- },
+	dependencies = { "rcarriga/nvim-dap-ui", "nvim-neotest/nvim-nio" },
+	cmd = {
+		"DapContinue",
+		"DapLoadLaunchJSON",
+		"DapRestartFrame",
+		"DapSetLogLevel",
+		"DapShowLog",
+		"DapStepInto",
+		"DapStepOut",
+		"DapStepOver",
+		"DapTerminate",
+		"DapToggleBreakpoint",
+		"DapToggleRepl",
+	},
+	keys = {
+		{ "<leader>db", "<cmd>DapToggleBreakpoint<cr>", desc = "Put Debug Breakpoint" },
+	},
 	config = function()
 		vim.keymap.set("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>")
+
+		local dap, dapui = require("dap"), require("dapui")
+		dapui.setup()
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			-- dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function() end
 	end,
 }
