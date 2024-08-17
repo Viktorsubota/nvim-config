ddf2ad0ab28ce438bf38cdb3095879bc40201e08
John Doe <junk1233messages@gmail.com>
2024-08-17 11:58:16 +0200

    Change tabs to spaces

--- lua/vsubota/plugins/dap-python.lua
+++ lua/vsubota/plugins/dap-python.lua
@@ -1,27 +1,27 @@
 return {
-	"mfussenegger/nvim-dap-python",
-	ft = { "python" },
-	dependencies = {
-		"mfussenegger/nvim-dap",
-		"rcarriga/nvim-dap-ui",
-	},
-	config = function()
-		local path = "/Users/viktorsubota/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-		require("dap-python").setup(path)
+    "mfussenegger/nvim-dap-python",
+    ft = { "python" },
+    dependencies = {
+        "mfussenegger/nvim-dap",
+        "rcarriga/nvim-dap-ui",
+    },
+    config = function()
+        local path = "/Users/viktorsubota/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+        require("dap-python").setup(path)
 
-		local configs = require("dap").configurations.python
-		table.insert(configs, {
-			name = "Pytest: Current File",
-			type = "python",
-			request = "launch",
-			module = "pytest",
-			args = {
-				"${file}",
-				"-sv",
-				"--log-cli-level=INFO",
-				"--log-file=test_out.log",
-			},
-			console = "integratedTerminal",
-		})
-	end,
+        local configs = require("dap").configurations.python
+        table.insert(configs, {
+            name = "Pytest: Current File",
+            type = "python",
+            request = "launch",
+            module = "pytest",
+            args = {
+                "${file}",
+                "-sv",
+                "--log-cli-level=INFO",
+                "--log-file=test_out.log",
+            },
+            console = "integratedTerminal",
+        })
+    end,
 }
