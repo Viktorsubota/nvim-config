ed73affd33635317570e38a89ebf4cb7fd234963
John Doe <junk1233messages@gmail.com>
2024-12-12 22:41:10 +0100

    Replace ruff_lsp with ruff as it's outdated

--- lua/vsubota/plugins/dap.lua
+++ lua/vsubota/plugins/dap.lua
@@ -1,38 +1,38 @@
 return {
-    "mfussenegger/nvim-dap",
-    dependencies = { "rcarriga/nvim-dap-ui", "nvim-neotest/nvim-nio" },
-    cmd = {
-        "DapContinue",
-        "DapLoadLaunchJSON",
-        "DapRestartFrame",
-        "DapSetLogLevel",
-        "DapShowLog",
-        "DapStepInto",
-        "DapStepOut",
-        "DapStepOver",
-        "DapTerminate",
-        "DapToggleBreakpoint",
-        "DapToggleRepl",
-    },
-    keys = {
-        { "<leader>db", "<cmd>DapToggleBreakpoint<cr>", desc = "Put Debug Breakpoint" },
-    },
-    lazy = true,
-    config = function()
-        vim.keymap.set("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>")
+	"mfussenegger/nvim-dap",
+	dependencies = { "rcarriga/nvim-dap-ui", "nvim-neotest/nvim-nio" },
+	cmd = {
+		"DapContinue",
+		"DapLoadLaunchJSON",
+		"DapRestartFrame",
+		"DapSetLogLevel",
+		"DapShowLog",
+		"DapStepInto",
+		"DapStepOut",
+		"DapStepOver",
+		"DapTerminate",
+		"DapToggleBreakpoint",
+		"DapToggleRepl",
+	},
+	keys = {
+		{ "<leader>db", "<cmd>DapToggleBreakpoint<cr>", desc = "Put Debug Breakpoint" },
+	},
+	lazy = true,
+	config = function()
+		vim.keymap.set("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>")
 
-        local dap, dapui = require("dap"), require("dapui")
-        dapui.setup()
+		local dap, dapui = require("dap"), require("dapui")
+		dapui.setup()
 
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-            -- dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function() end
-    end,
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			-- dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function() end
+	end,
 }
