14d4025ce167ac1e4766135c1c9a354501512248
John Doe <junk1233messages@gmail.com>
2024-12-15 02:03:59 +0100

    Tiny fixes

--- lua/vsubota/plugins/none-ls.lua
+++ lua/vsubota/plugins/none-ls.lua
@@ -24,99 +24,86 @@ local function find_pylintrc_in_git_root()
 end
 
 return {
-	{
+	"nvimtools/none-ls.nvim",
+	event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+	dependencies = {
 		"jay-babu/mason-null-ls.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"williamboman/mason.nvim",
-			"nvimtools/none-ls.nvim",
-		},
-		config = function()
-			local mason_null_ls = require("mason-null-ls")
-
-			mason_null_ls.setup({
-				ensure_installed = {
-					"prettier", -- prettier formatter
-					"stylua", -- lua formatter
-
-					"black", -- python formatter
-					"pylint", -- python linter
-					"debugpy",
-					"isort",
-					"ruff",
-
-					"gopls",
-					"gofmt",
-					"goimports",
-					"golines",
-					"delve",
-				},
-			})
-		end,
+		"williamboman/mason.nvim",
 	},
-	{
-		"nvimtools/none-ls.nvim", -- configure formatters & linters
-		event = { "BufReadPre", "BufNewFile" }, -- to enable uncomment this
-		dependencies = {
-			"jay-babu/mason-null-ls.nvim",
-		},
-		config = function()
-			local null_ls = require("null-ls")
+	config = function()
+		local null_ls = require("null-ls")
+		local null_ls_utils = require("null-ls.utils")
+		local mason_null_ls = require("mason-null-ls")
 
-			local null_ls_utils = require("null-ls.utils")
+		local formatting = null_ls.builtins.formatting -- to setup formatters
+		local diagnostics = null_ls.builtins.diagnostics -- to setup linters
 
-			local formatting = null_ls.builtins.formatting -- to setup formatters
-			local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		mason_null_ls.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
 
-			null_ls.setup({
+				"black", -- python formatter
+				"pylint", -- python linter
+				"debugpy",
+				"isort",
+				"ruff",
 
-				debug = true,
-				autostart = true,
+				"gopls",
+				"gofmt",
+				"goimports",
+				"golines",
+				"delve",
+			},
+		})
 
-				-- add package.json as identifier for root (for typescript monorepos)
-				root_dir = null_ls_utils.root_pattern("pyproject.toml", ".null-ls-root", "Makefile", ".git", "go.mod"),
-				-- setup formatters & linters
-				sources = {
-					--  to disable file types use
-					--  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
+		null_ls.setup({
+			debug = true,
+			autostart = true,
 
-					formatting.stylua, -- lua formatter
+			-- add package.json as identifier for root (for typescript monorepos)
+			root_dir = null_ls_utils.root_pattern("pyproject.toml", ".null-ls-root", "Makefile", ".git", "go.mod"),
+			-- setup formatters & linters
+			sources = {
+				--  to disable file types use
+				--  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
 
-					formatting.isort,
-					formatting.black.with({
-						extra_args = { "--line-length", "100" },
-					}),
-					diagnostics.pylint.with({
-						extra_args = { "--rcfile", find_pylintrc_in_git_root() or "" },
-					}),
+				formatting.stylua, -- lua formatter
 
-					formatting.gofmt,
-					formatting.goimports,
-					formatting.golines,
-				},
+				formatting.isort,
+				formatting.black.with({
+					extra_args = { "--line-length", "100" },
+				}),
+				diagnostics.pylint.with({
+					extra_args = { "--rcfile", find_pylintrc_in_git_root() or "" },
+				}),
 
-				-- configure format on save
-				on_attach = function(current_client, bufnr)
-					if current_client.supports_method("textDocument/formatting") then
-						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-						vim.api.nvim_create_autocmd("BufWritePre", {
-							group = augroup,
-							buffer = bufnr,
-							callback = function()
-								vim.lsp.buf.format({
-									filter = function(client)
-										--  only use null-ls for formatting instead of lsp server
-										return client.name == "null-ls"
-									end,
-									bufnr = bufnr,
-								})
-							end,
-						})
-					end
-				end,
-			})
-		end,
-	},
+				formatting.gofmt,
+				formatting.goimports,
+				formatting.golines,
+			},
+
+			-- configure format on save
+			on_attach = function(current_client, bufnr)
+				if current_client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({
+								filter = function(client)
+									--  only use null-ls for formatting instead of lsp server
+									return client.name == "null-ls"
+								end,
+								bufnr = bufnr,
+							})
+						end,
+					})
+				end
+			end,
+		})
+	end,
 }
