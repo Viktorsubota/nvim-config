22c2400957de7fecbcdba3254beb029a8a45b7d4
John Doe <junk1233messages@gmail.com>
2026-03-30 21:02:11 +0200

    Fix linter/pythinset up

---
+++ lua/vsubota/plugins/conform.lua
@@ -0,0 +1,25 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("conform").format({ async = true, lsp_format = "fallback" })
+			end,
+			desc = "Format buffer",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "ruff_organize_imports", "ruff_format" },
+			go = { "gofmt", "goimports", "golines" },
+		},
+		format_on_save = {
+			timeout_ms = 3000,
+			lsp_format = "fallback",
+		},
+	},
+}
