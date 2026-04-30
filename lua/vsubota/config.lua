6b0168e0d943914ed21bb819396a92f6a4e740a7
John Doe <junk1233messages@gmail.com>
2026-04-30 14:38:07 +0200

    Auto-equalize splits on terminal resize for tmux zoom/unzoom

--- lua/vsubota/config.lua
+++ lua/vsubota/config.lua
@@ -1,3 +1,10 @@
+-- Equalize splits on terminal resize (e.g. tmux zoom/unzoom)
+vim.api.nvim_create_autocmd("VimResized", {
+	callback = function()
+		vim.cmd("wincmd =")
+	end,
+})
+
 -- Highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking text",
